b3b4d2f53ea70956b8df71a846633fe103a5108b
ameed_f <ameed_f@malam.com>
2017-11-21 17:03:37 +0200

    Removing "final"

--- src/ameedf/jb8223/lectures/enums/DaysApplication.java
+++ src/ameedf/jb8223/lectures/enums/DaysApplication.java
@@ -6,8 +6,8 @@ public class DaysApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the day number: ");
-        final int dayNumber = scanner.nextInt();
-        final DayOfWeek day = DayOfWeek.getByDayNumber(dayNumber);
+        int dayNumber = scanner.nextInt();
+        DayOfWeek day = DayOfWeek.getByDayNumber(dayNumber);
         System.out.println("The day of the week is " + day);
 
         // REPLACE the day number with day short name (sun, mon, ...)
