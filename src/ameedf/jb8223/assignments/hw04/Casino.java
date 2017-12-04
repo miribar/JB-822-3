610b063660189a0950cb9be9bd69001b421ce006
ameed_f <ameed_f@malam.com>
2017-12-04 13:54:32 +0200

    HW-04

--- src/ameedf/jb8223/assignments/hw04/Casino.java
+++ src/ameedf/jb8223/assignments/hw04/Casino.java
@@ -1,8 +1,42 @@
 package ameedf.jb8223.assignments.hw04;
 
+/**
+ * A place where all entities meet.
+ * 1. Create an array that will hold at least 3 entities of each type (interface / class)
+ * 2. Initialize all entities
+ * 3. Create a simple test for the Dealer interface
+ * 4. Create a simple test for the Consumer interface
+ * <p>
+ * You should change the code so the tests will work
+ */
 public class Casino {
 
     public void start() {
+        // You can change the code
 
+        // 1
+        create();
+
+        // 2
+        initialize();
+
+        // 3
+        testDealerInterface();
+
+        // 4
+        testConsumerInterface();
+
+    }
+
+    private void create() {
+    }
+
+    private void initialize() {
+    }
+
+    private void testDealerInterface() {
+    }
+
+    private void testConsumerInterface() {
     }
 }
--- src/ameedf/jb8223/assignments/hw04/CasinoApplication.java
+++ src/ameedf/jb8223/assignments/hw04/CasinoApplication.java
@@ -7,12 +7,11 @@ package ameedf.jb8223.assignments.hw04;
  * Consumer: INTERFACE - an entity that can accept the act of dealing from a Dealer
  * Player: a Person who is also a Consumer (Consume the cards dealing)
  * CardsDealer: a Person who is also a Dealer. Has a list of players to deal to
- * DrugsDealer: a Person who is also a Dealer.
- * Has a list of AddictedPlayers to deal to and a specific amount of drugs to deal (in grams).
- * Each addict will consume 10g each time. When there are no drugs to deal, the dealing action will do nothing
- * Santa: a Dealer of toys. Has a limited number of toys to deal.
- * When there are no toys to deal, the dealing action will do nothing
+ * DrugsDealer: a Person who is also a Dealer. Has a list of AddictedPlayers to deal to and a specific amount of drugs to deal (in grams).
+ * Each addict will consume 10g each time. When there are no drugs to deal, the dealing action will do nothing.
+ * Santa: a Dealer of toys. Has a limited number of toys to deal. When there are no toys to deal, the dealing action will do nothing
  * AddictedPlayer: a player that can consume drugs (Consumer)
+ * Casino: a place that all those entities can meet. Check the class for more information
  */
 public class CasinoApplication {
     public static void main(String[] args) {
