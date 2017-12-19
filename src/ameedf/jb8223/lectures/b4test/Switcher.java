56f196158477f605c7c027a74c32fcae377abab0
ameed_f <ameed.lookup@gmail.com>
2017-12-19 10:41:30 +0200

    Some more classes and inheritance

--- src/ameedf/jb8223/lectures/b4test/Switcher.java
+++ src/ameedf/jb8223/lectures/b4test/Switcher.java
@@ -1,30 +1,30 @@
 package ameedf.jb8223.lectures.b4test;
 
 public class Switcher {
-	public static void main(String[] args) {
-		Switcher switcher = new Switcher();
-		switcher.doSwitch(2);
-	}
+    public static void main(String[] args) {
+        Switcher switcher = new Switcher();
+        switcher.doSwitch(4);
+    }
 
-	private void doSwitch(int number) {
-		switch (number) {
-		case 2:
-			number++;
-			System.out.println("A " + number);
-		case 9:
-		case 6:
-			number *=2;
-			break;
-		case 3:
-			System.out.println("B " + number);
-		case 5:
-		case 7:
-			System.out.println("C " + number);
-		default:
-			System.out.println("D " + number);
-			break;
-		}
+    private void doSwitch(int number) {
+        switch (--number) {
+            case 2:
+                number++;
+                System.out.println("A " + number);
+            case 9:
+            case 6:
+                number *= 2;
+                break;
+            case 3:
+                System.out.println("B " + ++number);
+            case 5:
+            case 7:
+                System.out.println("C " + number / 2);
+            default:
+                System.out.println("D " + number * 3);
+                break;
+        }
 
         System.out.println(number);
-	}
+    }
 }
