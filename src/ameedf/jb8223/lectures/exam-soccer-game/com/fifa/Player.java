a55ec59069239f91b4ac929d94ad6ffb60a245fb
ameedf <ameed.software@gmail.com>
2018-02-06 12:24:34 +0200

    Create Player.java

---
+++ src/ameedf/jb8223/lectures/exam-soccer-game/com/fifa/Player.java
@@ -0,0 +1,14 @@
+package com.fifa;
+
+public class Player {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
